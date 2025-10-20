a3c15401e3089b3c19bbc75100faec39898f6ed8
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-20 19:43:44 +0000

    Started project
    Co-authored-by: soappp12 <soappp12@users.noreply.github.com>
    Co-authored-by: Lecheconflakes <Lecheconflakes@users.noreply.github.com>
    Co-authored-by: koolaid-yay <koolaid-yay@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
+// Tic-Tac-Toe SP, YS, SB, AS
+//SP-player choice, AS- game end, SB-AI and Ys- board
+#include <stdio.h>
+#include <string.h>
+void board(void){
+    char board1[50] = {"-"};
+    char board2[50] = {"|"};
+    char board3[50];
 
+    strcat(board3, board1);
+    strcat(board3, board2);
+    strcat(board3, board1);
+    strcat(board3, board2);
+    strcat(board3, board1);
+    strcat(board3, board2);
+    printf("%s\n", board3);
+    printf("%s\n", board3);
+    printf("%s\n", board3);
+
+}
+
+int main(void){
+    char user_input[50];
+    printf("Will you pick X or O: ");
+    scanf("%c", user_input);
+    board();
+    return 0; 
+}
