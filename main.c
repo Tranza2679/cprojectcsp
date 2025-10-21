d96b7ce9beb07d2a4846d986dcc019d4eb786146
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-21 19:43:26 +0000

    Co-authored-by: Lecheconflakes <Lecheconflakes@users.noreply.github.com> Co-authored-by: soappp12 <soappp12@users.noreply.github.com> Co-authored-by: koolaid-yay <koolaid-yay@users.noreply.github.com>

--- main.c
+++ main.c
@@ -2,27 +2,47 @@
 //SP-player choice, AS- game end, SB-AI and Ys- board
 #include <stdio.h>
 #include <string.h>
-void board(void){
-    char board1[50] = {"-"};
-    char board2[50] = {"|"};
-    char board3[50];
+#include <time.h>
+#include <stdlib.h>
 
+// Yaretzi Sanchez board visuals.
+  void board(void){
+
+    char board1[] = {"_"};
+    char board2[] = {"|"};
+    char board3[10];
+
+    
     strcat(board3, board1);
     strcat(board3, board2);
     strcat(board3, board1);
     strcat(board3, board2);
     strcat(board3, board1);
-    strcat(board3, board2);
     printf("%s\n", board3);
     printf("%s\n", board3);
     printf("%s\n", board3);
+  }
 
-}
-
-int main(void){
+  
+void game_play(void){
     char user_input[50];
+    char ai_choice[50];
     printf("Will you pick X or O: ");
     scanf("%c", user_input);
+    if(user_input, "X"){
+        char ai_choice = "O";
+    } else if(user_input, "O"){
+        char ai_choice = "X";
+    }else{
+        printf("Please choose again!");
+    }
+    char game_end[6] = "False";
+    while(game_end != "False");
+    
+    
+    
+}
+int main(void){
     board();
     return 0; 
 }
